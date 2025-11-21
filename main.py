8569ab3d9845ef9594281afb4b34f71b0f496d76
Eren Saydar <erensaydar2000@gmail.com>
2025-11-22 01:34:16 +0300

    just different comments

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
 
     # PLEASE UNCOMMENT THE DESIRED FUNCTION ONLY (THERE ARE 4 OPTIONS)
 
-    toolpathing_2D.custom_toolpath_2D() #plots 2D custom toolpath
-    #toolpathing_2D.canned_toolpath_2D(start_x=4, start_y=4, length=4, step=2, turns=4) #plots 2D curved zigzag pattern
+    #toolpathing_2D.custom_toolpath_2D() #plots 2D custom toolpath
+    toolpathing_2D.canned_toolpath_2D(start_x=4, start_y=4, length=4, step=2, turns=4) #plots 2D curved zigzag pattern
     #toolpathing_3D.canned_toolpath_3D(radius=4, turns=5, height=10, x_center=5, y_center=0, z_center=0) #plots the helix in 3D
     #toolpathing_3D.canned_interactive_toolpath() #interactive helix with t value input for parameter equation changing with a slider
 
